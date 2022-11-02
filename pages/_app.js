7f69e5ad18be4ea0518a64d5269b76b218d4799b
Thelordgreat <greatokafor15@gmail.com>
2022-11-02 22:26:37 +0100

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import Footer from "../Components/Footer";
-//import Navbar from "../Components/Navbar";
+import Navbar from "../Components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Component {...pageProps} />
       <Footer/>
     </>
